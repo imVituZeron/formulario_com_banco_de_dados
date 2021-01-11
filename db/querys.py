b1235efb03f17b1af8e9c85e1dc5fd0122da229f
vitor santos <vitorpaulasantos@gmail.com>
2021-01-11 14:18:35 -0300

    criando uma context manager para cada query

--- db/querys.py
+++ db/querys.py
@@ -1,31 +1,49 @@
 import sqlite3
+from contextlib import contextmanager
 
-conncetion = sqlite3.connect('db/register.db')
-cursor = conncetion.cursor()
+@contextmanager
+def connect():
+    connection = sqlite3.connect('db/register.db')
+
+    try:
+        yield connection
+    finally:
+        connection.close()
 
 def insert(name, age, mail, phone):
-    query = "INSERT OR IGNORE INTO user (name, age, mail, phone) VALUES (?, ?, ?, ?)"
-    cursor.execute(query, (name, age, mail, phone))
-    conncetion.commit()
+    with connect() as connection:
+        with connection.cursor() as cursor:
+            query = "INSERT OR IGNORE INTO user (name, age, mail, phone) VALUES (?, ?, ?, ?)"
+            cursor.execute(query, (name, age, mail, phone))
+            connection.commit()
+
     return True
 
 def list_data():
-    query = "SELECT * FROM user"
-    cursor.execute(query)
+    with connect() as connection:
+        with connection.cursor() as cursor:
+            query = "SELECT * FROM user"
+            cursor.execute(query)
 
-    for date in cursor.fetchall():
-        print(date)
+            for date in cursor.fetchall():
+                print(date)
 
 def update(data_table, value, identification):
-    query = f"UPDATE user SET {data_table}=? WHERE id=?"
-    cursor.execute(query, (value, identification))
-    conncetion.commit()
+    with connect() as connection:
+        with connection.cursor() as cursor:
+            query = f"UPDATE user SET {data_table}=? WHERE id=?"
+            cursor.execute(query, (value, identification))
+            connection.commit()
+
     return True
 
 def delete(identification):
-    query = "DELETE FROM user WHERE id=?"
-    cursor.execute(query, (identification,))
-    conncetion.commit()
+    with connect() as connection:
+        with connection.cursor() as cursor:
+            query = "DELETE FROM user WHERE id=?"
+            cursor.execute(query, (identification,))
+            connection.commit()
+            
     return True
 
 
