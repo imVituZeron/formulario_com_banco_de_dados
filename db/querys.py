614fdbd9f68f64964351ea3ea5d34a04804f28f1
vitor santos <vitorpaulasantos@gmail.com>
2021-01-11 15:25:33 -0300

    tirando os context_managers e criando as querys de UPDATE

--- db/querys.py
+++ db/querys.py
@@ -1,49 +1,77 @@
 import sqlite3
 from contextlib import contextmanager
 
-@contextmanager
-def connect():
-    connection = sqlite3.connect('db/register.db')
 
-    try:
-        yield connection
-    finally:
-        connection.close()
+connection = sqlite3.connect('db/register.db')
+cursor = connection.cursor()
+
 
 def insert(name, age, mail, phone):
-    with connect() as connection:
-        with connection.cursor() as cursor:
-            query = "INSERT OR IGNORE INTO user (name, age, mail, phone) VALUES (?, ?, ?, ?)"
-            cursor.execute(query, (name, age, mail, phone))
-            connection.commit()
+    query = "INSERT OR IGNORE INTO user (name, age, mail, phone) VALUES (?, ?, ?, ?)"
+    cursor.execute(query, (name, age, mail, phone))
+    connection.commit()
 
     return True
+
 
 def list_data():
-    with connect() as connection:
-        with connection.cursor() as cursor:
-            query = "SELECT * FROM user"
-            cursor.execute(query)
+    query = "SELECT * FROM user"
+    cursor.execute(query)
 
-            for date in cursor.fetchall():
-                print(date)
+    for date in cursor.fetchall():
+        print(date)
 
-def update(data_table, value, identification):
-    with connect() as connection:
-        with connection.cursor() as cursor:
-            query = f"UPDATE user SET {data_table}=? WHERE id=?"
-            cursor.execute(query, (value, identification))
-            connection.commit()
+
+def get_date_select(value):
+    query = "SELECT * FROM user"
+    cursor.execute(query)
+
+    for date in cursor.fetchall():
+        ID, *othes = date
+
+        if ID == int(value):
+            return True
+        else:
+            return False
+
+
+def update_name(value, identification):
+    query = "UPDATE user SET name=? WHERE id=?"
+    cursor.execute(query, (value, identification))
+    connection.commit()
 
     return True
 
+
+def update_age(value, identification):
+    query = "UPDATE user SET age=? WHERE id=?"
+    cursor.execute(query, (value, identification))
+    connection.commit()
+
+    return True
+
+
+def update_mail(value, identification):
+    query = "UPDATE user SET mail=? WHERE id=?"
+    cursor.execute(query, (value, identification))
+    connection.commit()
+
+    return True
+
+
+def update_phone(value, identification):
+    query = "UPDATE user SET phone=? WHERE id=?"
+    cursor.execute(query, (value, identification))
+    connection.commit()
+
+    return True
+
+
 def delete(identification):
-    with connect() as connection:
-        with connection.cursor() as cursor:
-            query = "DELETE FROM user WHERE id=?"
-            cursor.execute(query, (identification,))
-            connection.commit()
-            
+    query = "DELETE FROM user WHERE id=?"
+    cursor.execute(query, (identification,))
+    connection.commit()
+
     return True
 
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from db.querys import insert, list_data, delete
+from db.querys import update_name, update_age, update_mail, update_phone, get_date_select
 
 while True:
+    print('=-='* 10)
     print('Welcome to Vitor´s databases')
     print(
         """
@@ -10,7 +12,7 @@ while True:
         4 -  Delete registration
         5 -  Exit
         """
-        )
+    )
     op = int(input("Qual das opções acima voce escolheu? "))
     if op == 1:
         name = str(input('Digite seu nome: '))
@@ -21,6 +23,53 @@ while True:
             print('Criado o cadastro com sucesso!')
         else:
             print('Ocorreu um erro!')
+
+    elif op == 2:
+        print('=-='* 10)
+        print(
+            """
+            1 - nome
+            2 - idade
+            3 - email
+            4 - telefone
+            """
+        )
+        list_ops = ['1', '2', '3', '4']
+        field_value = input('Digite o numero que corresponde com o campo que deseja modificar: ')
+        
+        if field_value.isdigit() and field_value in list_ops:
+            identification = input('Digite o id do cadastro que deseja modificar: ')
+            if identification.isdigit() and get_date_select(identification):
+                value = input('Digite o valor que quer colocar no campo escolhido: ')
+                if field_value == '1':
+                    if update_name(value, int(identification)):
+                        print('Modificação feita com sucesso!')
+                    else:
+                        print('Ocorreu um erro!')
+
+                elif field_value == '2':
+                    if value.isdigit():
+                        if update_age(int(value), int(identification)):
+                                print('Modificação feita com sucesso!')
+                        else:
+                            print('Ocorreu um erro!')   
+
+                elif field_value == '3':
+                    if update_mail(value, int(identification)):
+                         print('Modificação feita com sucesso!')
+                    else:
+                        print('Ocorreu um erro!')
+                    
+                elif field_value == '4':
+                    if update_phone(value, int(identification)):
+                        print('Modificação feita com sucesso!')
+                    else:
+                        print('Ocorreu um erro!')
+                
+            else:
+                print('Identificador desconhecido')
+        else:
+            print('Opção desconhecida') 
 
     elif op == 4:
         print('=-='* 10)
